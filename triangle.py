55ebb72fe01acab218e6fb9cfb0a5c9d926d8652
Artem <artem.dzichkanets@mail.ru>
2024-10-02 22:07:27 +0300

    adding functions docs to rectangle.py and triangle.py

--- triangle.py
+++ triangle.py
@@ -1,5 +1,10 @@
-def area(a, h): 
+def area(a, h):
+    """Returns area of a triangle with given sides.
+            Parameters:
+                a (int): length of a side
+                h (int): length of height passed to the side a"""
     return a * h / 2 
+
 
 def perimeter(a, b, c): 
     return a + b + c 
