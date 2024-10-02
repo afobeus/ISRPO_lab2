50265f5638725e2b0c11aa1c09cc906c3fedf02e
Artem <artem.dzichkanets@mail.ru>
2024-10-02 21:48:14 +0300

    adding functions docs for circle.py and square.py

--- circle.py
+++ circle.py
@@ -2,9 +2,11 @@ import math
 
 
 def area(r):
+    """Returns area of a circle with given radius r"""
     return math.pi * r * r
 
 
 def perimeter(r):
+    """Returns perimeter of a circle with given radius r"""
     return 2 * math.pi * r
 
