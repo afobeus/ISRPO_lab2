2f6aa6073169e014d10f21c0632ace67061f214e
Artem <artem.dzichkanets@mail.ru>
2024-10-02 23:32:41 +0300

    triangle.py function docs fix

--- triangle.py
+++ triangle.py
@@ -1,10 +1,13 @@
 def area(a, h):
-    """Returns area of a triangle with given sides.
+    """Returns area of a triangle with given side and height.
             Parameters:
                 a (int): length of a side
                 h (int): length of height passed to the side a"""
     return a * h / 2 
 
 
-def perimeter(a, b, c): 
+def perimeter(a, b, c):
+    """Returns perimeter of a triangle with given sides.
+            Parameters:
+                a, b, c (int): length of three sides"""
     return a + b + c 
